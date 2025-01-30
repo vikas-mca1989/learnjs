fe01e15db2944ab845e3ad516721dab654e662f1
vikas <vikas.sharma@redlizardstudioz.com>
2025-01-30 20:48:05 +0530

    conversion of datatypes

---
+++ 01_basic/03datatype.js
@@ -0,0 +1,9 @@
+// let name ="33";
+// console.log(name);
+// let newvar = Number(name);
+// console.log(newvar);
+// console.log(typeof(newvar));
+
+let logedIn = 1;
+let booleanIsLogedIn = Boolean(logedIn);
+console.log(booleanIsLogedIn);
